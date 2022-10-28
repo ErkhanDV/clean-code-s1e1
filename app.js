ffce81ffb1defe10936d7aaeba61ee8a33951799
ErkhanDV <erhan.denis@list.ru>
2022-10-29 00:34:43 +0500

    refactor: HTML-formating according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="todo-task__button-delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="delete-button-img";
     deleteButton.appendChild(deleteButtonImg);
 
 
