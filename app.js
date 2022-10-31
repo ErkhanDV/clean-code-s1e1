68a9de5ec2053e733fc2ca2934ce22d63a54b00b
ErkhanDV <erhan.denis@list.ru>
2022-10-31 14:17:01 +0500

    refactor: HTML-formatting (add Alternative attributes) according to rule 1.2 html-and-css-extended

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="todo-task__button-delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt='Remove button icon';
     deleteButtonImg.className="delete-button-img";
     deleteButton.appendChild(deleteButtonImg);
 
